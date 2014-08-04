efbfdae0ceabf4e01b07d69e8194db74e3302d04
ZhiHeng NIU <niuzhiheng@gmail.com>
2014-08-04 10:49:26 +0800

    Update the mnist downloading script. It seems dropbox has deprecated the http way. It can only be downloaded by wget with https support.

--- examples/mnist/get_mnist_leveldb.bat
+++ examples/mnist/get_mnist_leveldb.bat
@@ -1,2 +1,3 @@
-"../../tools/wget" http://dl.dropboxusercontent.com/u/3466743/caffe-vs2012/mnist-leveldb.7z
+"../../tools/wget" --no-check-certificate https://dl.dropboxusercontent.com/u/3466743/caffe-vs2012/mnist-leveldb.7z
 "../../tools/7za" x mnist-leveldb.7z
+pause
